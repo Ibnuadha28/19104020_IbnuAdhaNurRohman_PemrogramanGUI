1e8dd6a59bda011af29521b2c5205e98f932d8ea
Ibnuadha28 <72426221+Ibnuadha28@users.noreply.github.com>
2021-05-24 01:06:11 +0700

    Add files via upload

---
+++ Modul 4/Pengenalan PyQT/Tutup Form/MainForm.py
@@ -0,0 +1,36 @@
+from PyQt5.QtWidgets import QWidget, QPushButton
+from OtherForm import *
+
+class MainForm(QWidget):
+	def __init__(self):
+		super().__init__()
+		self.setupUi()
+
+	def setupUi(self):
+		self.resize(300, 200)
+		self.move(300, 300)
+		self.setWindowTitle('Demo Menampilkan Form')
+
+		self.button = QPushButton('Tampilkan Form Lain')
+		self.button.move(50, 50)
+		self.button.setParent(self)
+
+		self.button.clicked.connect(self.buttonClick)
+
+	def buttonClick(self):
+		self.close()
+		self.form = OtherForm()
+		self.form.show()
+
+	def buttonClick1(self):
+		self.close()
+
+import sys
+from PyQt5.QtWidgets import QApplication
+from MainForm import *
+
+if __name__ == '__main__':
+	a = QApplication(sys.argv)
+	form = MainForm()
+	form.show()
+	a.exec_()
